9c7c9d4edfcccf44850c8920272f42e51bfa7e3b
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2026-05-08 17:50:21 +0800

    fix products

--- src/features/products/queries/use-get-products-query.tsx
+++ src/features/products/queries/use-get-products-query.tsx
@@ -1,15 +1,12 @@
+import { api } from "@/services/api/axios";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useGetProductsQuery() {
   return useQuery({
     queryKey: ["products"],
     queryFn: async () => {
-      // Replace with your API call
-      const response = await fetch("/api/products");
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
+      const { data } = await api.get("/products");
+      return data;
     },
   });
 }
