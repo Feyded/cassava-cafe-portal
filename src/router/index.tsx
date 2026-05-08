671611f4b0a3dd353afe9d490b6de534c7bfad2d
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2026-05-08 17:57:59 +0800

    feat: add routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,3 +1,5 @@
+import AdminDashboardPage from "@/features/admin/dashboard/pages/admin-dashboard-page";
+import AdminProductsPage from "@/features/admin/products/pages/admin-products-page";
 import LoginPage from "@/features/auth/pages/login-page";
 import HomePage from "@/features/home/pages/home-page";
 import ProductsPage from "@/features/products/pages/products-page";
@@ -11,6 +13,15 @@ export const router = createBrowserRouter([
           { path: "/", element: <HomePage /> },
           { path: "/products", element: <ProductsPage /> },
           { path: "/login", element: <LoginPage /> },
+        ],
+      },
+
+      //ADMIN ROUTES
+      {
+        path: "/admin",
+        children: [
+          { path: "dashboard", element: <AdminDashboardPage /> },
+          { path: "products", element: <AdminProductsPage /> },
         ],
       },
     ],
