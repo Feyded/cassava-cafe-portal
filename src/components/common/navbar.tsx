d2d30916d33ec854deb1963df6cc2f001accc33a
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2026-05-21 14:39:49 +0800

    fix: navbar

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,3 +1,5 @@
+import useLogoutMutation from "@/features/auth/queries/use-logout-mutation";
+import useMeQuery from "@/features/me/queries/use-me-query";
 import { Coffee } from "lucide-react";
 import { Link } from "react-router-dom";
 
@@ -7,7 +9,11 @@ const navLinks = [
 ];
 
 export default function Navbar() {
-  return (
+  const me = useMeQuery();
+
+  const logout = useLogoutMutation();
+
+    return (
     <header className="sticky top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur">
       <nav className="mx-auto flex h-16 max-w-6xl items-center justify-between px-6">
         <Link to="/" className="flex items-center gap-2.5">
@@ -28,6 +34,25 @@ export default function Navbar() {
               </Link>
             </li>
           ))}
+          {me.data ? (
+            <li key="Logout">
+              <button
+                onClick={() => logout.mutate()}
+                className="text-xs font-semibold uppercase tracking-widest text-muted-foreground transition-colors hover:text-foreground"
+              >
+                Logout
+              </button>
+            </li>
+          ) : (
+            <li key="Login">
+              <Link
+                to="/login"
+                className="text-xs font-semibold uppercase tracking-widest text-muted-foreground transition-colors hover:text-foreground"
+              >
+                Login
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
---
+++ src/features/auth/queries/use-logout-mutation.tsx
@@ -0,0 +1,16 @@
+import { api } from "@/services/api/axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
+export default function useLogoutMutation() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  return useMutation({
+    mutationFn: async () => {
+      localStorage.removeItem("auth_token");
+      queryClient.clear();
+      navigate("/login");
+      await api.post("/logout");
+    },
+  });
+}
