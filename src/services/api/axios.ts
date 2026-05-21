9f8698bccde1987e0da7115b2f6495594aa225d7
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2026-05-21 13:54:55 +0800

    feat: apply token token when exist in local storage

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -3,3 +3,15 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("auth_token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    delete config.headers.Authorization;
+  }
+
+  return config;
+});
