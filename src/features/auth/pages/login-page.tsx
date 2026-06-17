05764049d5ad0bcf76b31bac284d34da45169e72
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2026-06-17 14:12:02 +0800

    feat: error message axios

--- src/features/auth/pages/login-page.tsx
+++ src/features/auth/pages/login-page.tsx
@@ -23,6 +23,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import type { AxiosError } from "axios";
 
 const highlights = [
   "Track orders and product availability in one place.",
@@ -74,8 +75,9 @@ export default function LoginPage() {
 
       navigate("/admin/dashboard");
     } catch (error) {
+      const err = error as AxiosError<{ message: string }>;
       const message =
-        error.response?.data?.message || "Login failed. Please try again.";
+        err.response?.data?.message || "Login failed. Please try again.";
       setErrorMessage(message);
     }
   };
