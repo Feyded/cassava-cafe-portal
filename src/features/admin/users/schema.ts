4d9d18d6a67a9d89f66a97236cbe35ff2120838c
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2026-06-25 10:22:21 +0800

    feat: schema

--- src/features/admin/users/schema.ts
+++ src/features/admin/users/schema.ts
@@ -1,11 +1,23 @@
 import z from "zod";
 
 export const createUserSchema = z.object({
-  first_name: z.string().min(1).max(60),
-  middle_name: z.string().max(60),
-  last_name: z.string().min(1).max(60),
-  role: z.string().min(1).max(60),
-  email: z.email().max(100),
+  first_name: z
+    .string()
+    .min(1, "First name is required")
+    .max(60, "First name cannot exceed 60 characters"),
+  middle_name: z
+    .string()
+    .max(60, "Middle name cannot exceed 60 characters")
+    .optional(),
+  last_name: z
+    .string()
+    .min(1, "Last name is required")
+    .max(60, "Last name cannot exceed 60 characters"),
+  role: z
+    .string()
+    .min(1, "Role is required")
+    .max(60, "Role cannot exceed 60 characters"),
+  email: z.email().max(100, "Email cannot exceed 100 characters"),
   is_active: z.boolean(),
   password: z.string().optional(),
 });
